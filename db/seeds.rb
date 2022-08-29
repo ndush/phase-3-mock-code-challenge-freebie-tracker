d4f65fba887d2ba96558fabb31b4475e04e27fd7
damaris <mdamarisnduku@gmail.com>
2022-08-29 13:23:06 +0300

    Lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+co_1 = Company.create(name: "Google", founding_year: 1998)
+co_2 = Company.create(name: "Facebook", founding_year: 2004)
+co_3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+co_4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev_1 = Dev.create(name: "Rick")
+dev_2 = Dev.create(name: "Morty")
+dev_3 = Dev.create(name: "Mr. Meseeks")
+dev_4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Shirt", value: 500, company_id:co_1.id, dev_id: dev_1.id)
+Freebie.create(item_name: "Mug", value: 200, company_id:co_4.id, dev_id: dev_1.id)
+Freebie.create(item_name: "Notebook", value: 250, company_id:co_2.id, dev_id: dev_3.id)
+Freebie.create(item_name: "Pen", value: 100, company_id:co_2.id, dev_id: dev_3.id)
 
 puts "Seeding done!"
